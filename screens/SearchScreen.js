288ed8afc43c7d6190bf6c21b73c91ca43d925a4
Valentine Lavaly <valentinelavaly@AirdeValentine.concorde-pereire.loc>
2021-12-06 10:56:26 +0100

    creation header logo + connexion

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { StyleSheet , Text, View, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Button, Image } from 'react-native-elements';
+
 
 function SearchScreen(props) {
     return (
         <View style={styles.container}>
+            <View style = {styles.header}>
+                <Image 
+                source = {require('../assets/tattoo-moi_1.png')}
+                style={{ width: 200, height: 80, marginRight: 70 }} />
+            <Button
+            title="Connexion"
+            buttonStyle = {{backgroundColor:'#424D41', padding:1, paddingRight:5, paddingLeft:5, borderRadius:5}}
+            type="solid"
+            />
+            </View>
             
+<View style = {styles.main}>
             <Button 
             title="Rechercher"
             type="solid"
-            color = '#424D41'
+            buttonStyle = {{backgroundColor : '#424D41'}}
             onPress={() => props.navigation.navigate('Resultat')}
      />
+     </View>
         </View>
     )
 }
@@ -20,9 +34,24 @@ export default SearchScreen;
 const styles = StyleSheet.create({
     container: {
       flex: 1,
+      flexDirection : 'column',
+      paddingTop : 50,
       backgroundColor: '#F1EFE5',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    header: {
+        flex:2,  
+        maxHeight : 80,
+        flexDirection : 'row',
+        alignItems : 'center',
+       justifyContent :'space-evenly',
+    },
+    main: {
+        flex:3,
+        flexDirection : 'column',  
+        alignItems : 'center',
+        justifyContent: 'center',
     },
     });
     
